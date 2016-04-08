79800a9d8e99c0ed0f68ea35984ebe5345eb99bf
Kyle Barbary <kylebarbary@gmail.com>
2016-04-08 06:48:37 -0700

    add logo, setup, README

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name="nickel", 
+      description="Next-gen SN models library",
+      long_description="",
+      classifiers = ["Development Status :: 1 - Planning",
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Topic :: Scientific/Engineering",
+                     "Topic :: Scientific/Engineering :: Astronomy",
+                     "Intended Audience :: Science/Research"],
+      url="http://github.com/kbarbary/nickel",
+      author="Kyle Barbary",
+      author_email="kylebarbary@gmail.com")
